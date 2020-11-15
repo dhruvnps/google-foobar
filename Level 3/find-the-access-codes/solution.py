4bfcfe8bde03a00d7275d23c00088973b39c892c
dhruvpattem <dhruv.pattem@gmail.com>
2020-11-15 17:11:36 +0000

    edits

--- Level 3/find-the-access-codes/solution.py
+++ Level 3/find-the-access-codes/solution.py
@@ -12,23 +12,3 @@ def solution(l):
 print(solution([1, 2, 3, 4, 5, 6]))
 print(solution([1, 1, 1]))
 print(solution([1] * 2000))
-
-
-# def solution(l):
-#     c = 0
-#     for x, i in enumerate(l[:-2]):
-#         c += triples(l, idx=x, pos=1)
-#     return c
-#
-#
-# def triples(l, idx, pos):
-#     if pos == 3:
-#         return 1
-#     c = 0
-#     nxt_idx = idx+1
-#     nxt_l = l[nxt_idx:]
-#     if len(nxt_l) > 0:
-#         for x, i in enumerate(nxt_l):
-#             if i >= l[idx] and i % l[idx] == 0:
-#                 c += triples(l, x + nxt_idx, pos + 1)
-#     return c
