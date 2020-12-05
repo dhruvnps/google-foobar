ba4dbc56a0fe55d6303c16d85549f24ef21a3ada
dhruvpattem <dhruv.pattem@gmail.com>
2020-12-05 01:43:37 +0000

    passes all test cases

--- Level 4/bringing-a-gun-to-a-guard-fight/solution.py
+++ Level 4/bringing-a-gun-to-a-guard-fight/solution.py
@@ -1,45 +1,34 @@
-def f(dimensions, a, b, distance):
-    d2 = distance ** 2
-    w, h = [(b[i], dimensions[i] - b[i]) for i in [0, 1]]
-    x, y = b
+import math
+
+
+def solution(dims, a, b, dist):
+    a_mirr = mirr_vals(dist, dims, a)
+    b_mirr = mirr_vals(dist, dims, b)
+    vectors = []
+    dists = {}
+    for i in [0, 1]:
+        for x in [a_mirr, b_mirr][i][0]:
+            for y in [a_mirr, b_mirr][i][1]:
+                d = ((x - a[0]) ** 2 + (y - a[1]) ** 2)
+                v = math.atan2(y - a[1], x - a[0])
+                if d <= dist ** 2 and d != 0:
+                    if not (v in dists and dists[v] <= d):
+                        dists[v] = d
+                        if i == 1:
+                            vectors.append(v)
+    return len(set(vectors))
+
+
+def mirr_vals(dist, dims, pt):
+    w, h = [(pt[i], dims[i] - pt[i]) for i in [0, 1]]
+    x, y = pt
     x_vals, y_vals = [x, -x], [y, -y]
-    for i in range(-1, distance // min(dimensions)):
+    for i in range(-1, dist // min(dims)):
         x += w[i & 1] * 2
         y += h[i & 1] * 2
         x_vals.extend([x, -x])
         y_vals.extend([y, -y])
-    vectors = []
-    for x in x_vals:
-        for y in y_vals:
-            d = ((x - a[0]) ** 2 + (y - a[1]) ** 2)
-            if d < d2:
-                vector = (x - a[0], y - a[1])
-                if not any(equals(v, vector) for v in vectors):
-                    vectors.append(vector)
-    return vectors
-
-
-def equals(m, n):
-    if m[0] == n[0] == 0 or m[1] == n[1] == 0:
-        return True
-    if n[0] == 0 or n[1] == 0:
-        return False
-    if not (m[1] ^ n[1] >= 0 and m[0] ^ n[0] >= 0):
-        return False
-    return m[0] / float(n[0]) == m[1] / float(n[1])
-
-
-def solution(dimensions, a, b, distance):
-    l1 = f(dimensions, a, b, distance)
-    l2 = f(dimensions, a, a, distance)
-
-    c = 0
-    for i in l2:
-        for j in l1:
-            if equals(i, j) and i != (0, 0) and j != (0, 0) and abs(i[0]) <= abs(j[0]):
-                c += 1
-
-    return len(l1) - c
+    return x_vals, y_vals
 
 
 print(solution([3, 2], [1, 1], [2, 1], 4))  # 7
@@ -47,34 +36,3 @@ print(solution([300, 275], [150, 150], [185, 100], 500))  # 9
 print(solution([2, 5], [1, 2], [1, 4], 11))  # 27
 print(solution([23, 10], [6, 4], [3, 2], 23))  # 8
 print(solution([10, 10], [4, 4], [3, 3], 5000))  # 739323
-
-'''
-def solution(dimensions, a, b, distance):
-    w, h = [(b[i], dimensions[i] - b[i]) for i in [0, 1]]
-    x, y = b
-    x_vals, y_vals = [x, -x], [y, -y]
-    for i in range(-1, distance // min(dimensions)):
-        x += w[i & 1] * 2
-        y += h[i & 1] * 2
-        x_vals.extend([x, -x])
-        y_vals.extend([y, -y])
-    vectors = []
-    for x in x_vals:
-        for y in y_vals:
-            d = ((x - a[0]) ** 2 + (y - a[1]) ** 2) ** 0.5
-            if d < distance:
-                vector = (x - a[0], y - a[1])
-                if not any(equals(v, vector) for v in vectors):
-                    vectors.append(vector)
-    return len(vectors)
-
-
-def equals(m, n):
-    if m[0] == n[0] == 0 or m[1] == n[1] == 0:
-        return True
-    if n[0] == 0 or n[1] == 0:
-        return False
-    if not (m[1] ^ n[1] >= 0 and m[0] ^ n[0] >= 0):
-        return False
-    return m[0] / float(n[0]) == m[1] / float(n[1])
-'''
