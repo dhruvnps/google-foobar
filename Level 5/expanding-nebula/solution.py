8b8b5a22f9dc3e30444aac2fcf804e055262f51d
dhruvpattem <dhruv.pattem@gmail.com>
2021-01-04 23:33:24 +0000

    still fails tests 6 and 7, bitwise

--- Level 5/expanding-nebula/solution.py
+++ Level 5/expanding-nebula/solution.py
@@ -5,7 +5,8 @@ _ = False
 
 
 def solution(g):
-    nodes_list = [preimages(col) for col in cols(g)]
+    h = len(g)
+    nodes_list = [preimages(col, h) for col in cols(g)]
     c = [0]
     dfs(None, nodes_list, 0, c)
     return c[0]
@@ -20,36 +21,45 @@ def dfs(node, nodes_list, depth, c):
             dfs(nxt_node, nodes_list, depth + 1, c)
 
 
-def preimages(col):
+def preimages(col, h):
     pre_list = []
-    l = len(col) + 1
-    for n in range(2 ** (l * 2)):
-        b = format(n, 'b').zfill(l * 2)
-        pre = [b[:l], b[l:]]
-        if check(pre, col):
-            pre_list.append(pre)
+    for n1 in range(2 ** (h + 1)):
+        for n2 in range(2 ** (h + 1)):
+            pre = [n1, n2]
+            if check(pre, col, h):
+                pre_list.append(pre)
     return pre_list
 
 
 def cols(g):
     cols = []
     for idx in range(len(g[0])):
-        cols.append('')
+        n = 0
         for i in g:
-            cols[idx] += '1' if i[idx] else '0'
+            n = (n << 1) + i[idx]
+        cols.append(n)
     return cols
 
 
-def check(pre, col):
-    for idx, i in enumerate(col):
-        c = sum([int(pre[0][idx]),
-                 int(pre[1][idx]),
-                 int(pre[0][idx + 1]),
-                 int(pre[1][idx + 1])])
-        if int(i) != (c == 1):
+def check(pre, col, h):
+    for idx in range(h):
+        c = ((pre[0] >> idx & 1) +
+             (pre[1] >> idx & 1) +
+             (pre[0] >> (idx + 1) & 1) +
+             (pre[1] >> (idx + 1) & 1))
+        if col >> idx & 1 != (c == 1):
             return False
     return True
 
+
+# 4
+t1 = time.perf_counter()
+print(solution(
+    [[O, _, O],
+     [_, O, _],
+     [O, _, O]]))
+t2 = time.perf_counter()
+print('{:0.3} seconds'.format(t2 - t1))
 
 # 11567
 t1 = time.perf_counter()
@@ -58,15 +68,6 @@ print(solution(
      [O, O, _, _, _, _, O, O, O, _],
      [O, O, _, _, _, _, _, _, _, O],
      [_, O, _, _, _, _, O, O, _, _]]))
-t2 = time.perf_counter()
-print('{:0.3} seconds'.format(t2 - t1))
-
-# 4
-t1 = time.perf_counter()
-print(solution(
-    [[O, _, O],
-     [_, O, _],
-     [O, _, O]]))
 t2 = time.perf_counter()
 print('{:0.3} seconds'.format(t2 - t1))
 
