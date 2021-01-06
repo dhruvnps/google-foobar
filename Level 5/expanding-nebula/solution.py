69a71b2e0a00e26be2d715509e93e9b9c372c1a5
dhruvpattem <dhruv.pattem@gmail.com>
2021-01-06 18:52:03 +0000

    still works

--- Level 5/expanding-nebula/solution.py
+++ Level 5/expanding-nebula/solution.py
@@ -5,13 +5,19 @@ _ = False
 
 
 def solution(g):
-    return dfs(nodes_list(cols(g), len(g)), {})
+    cols = []
+    for col in zip(*g):
+        n = 0
+        for i in col:
+            n = n << 1 | i
+        cols.append(n)
+    return dfs(nodes_list(cols, len(g)), {})
 
 
-def dfs(nodes_list, mem, depth=0, node=[]):
+def dfs(nodes_list, mem, depth=0, node=()):
     if depth == len(nodes_list):
         return 1
-    key = tuple([depth] + node)
+    key = (depth,) + node
     if key in mem:
         return mem[key]
     mem[key] = 0
@@ -25,32 +31,22 @@ def nodes_list(cols, h):
     nodes_list = [[] for _ in cols]
     for n1 in range(2 ** (h + 1)):
         for n2 in range(2 ** (h + 1)):
-            nxt_col = nxt([n1, n2], h)
+            img = col_img((n1, n2), h)
             for idx, col in enumerate(cols):
-                if col == nxt_col:
-                    nodes_list[idx].append([n1, n2])
+                if col == img:
+                    nodes_list[idx].append((n1, n2))
     return nodes_list
 
 
-def nxt(prev, h):
+def col_img(pre, h):
     n = 0
     for idx in range(h):
-        c = ((prev[0] >> idx & 1) +
-             (prev[1] >> idx & 1) +
-             (prev[0] >> (idx + 1) & 1) +
-             (prev[1] >> (idx + 1) & 1))
+        c = ((pre[0] >> idx & 1) +
+             (pre[1] >> idx & 1) +
+             (pre[0] >> (idx + 1) & 1) +
+             (pre[1] >> (idx + 1) & 1))
         n = (n << 1) + (c == 1)
     return n
-
-
-def cols(g):
-    cols = []
-    for col in zip(*g):
-        n = 0
-        for i in col:
-            n = n << 1 | i
-        cols.append(n)
-    return cols
 
 
 # 4
